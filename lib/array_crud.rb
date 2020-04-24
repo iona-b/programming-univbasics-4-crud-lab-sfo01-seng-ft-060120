5b5435ab67866209e4e619ff097a4922d86764a5
Iona <ionabrabender@gmail.com>
2020-04-24 22:53:44 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  fruit = [apple, banana, canteloupe, dragonfruit]
+  array = [apple, banana, canteloupe, dragonfruit]
 end
 
 def add_element_to_end_of_array(array, element)
