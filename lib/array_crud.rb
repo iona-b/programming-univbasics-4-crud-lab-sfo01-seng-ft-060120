0a506e50da86b3921c377ff4ab523c016974d507
Iona <ionabrabender@gmail.com>
2020-04-24 22:57:24 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -15,23 +15,23 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array.unshift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  p array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  p array [0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  p array.delete([array.length - 1])
 end
 
 def update_element_from_index(array, index_number, element)
